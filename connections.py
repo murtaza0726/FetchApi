f09b82d9411a579a10ab51b690a7f69097d87996
Murtaza0726 <murtazanaqvi0726@yahoo.com>
2022-04-02 16:22:22 -0400

    new commit 1

--- connections.py
+++ connections.py
@@ -3,10 +3,4 @@ from flaskext.mysql import MySQL
 app = Flask(__name__)
 mysql = MySQL()
 
-app.config['MYSQL_DATABASE_HOST'] = "localhost"
-app.config['MYSQL_DATABASE_USER'] = "root"
-app.config['MYSQL_DATABASE_PASSWORD'] = "murtazahaider"
-app.config['MYSQL_DATABASE_DB'] = "Demo"
-app.config['MYSQL_DATABASE_PORT'] = 3306
-
 mysql.init_app(app)
